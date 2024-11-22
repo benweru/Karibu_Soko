528b37e15be6fdeb42909ebbabd2a2dcbcac1de7
benweru <benweru80@gmail.com>
2024-11-22 20:44:12 +0300

    Fixed errors and tested.

--- app/src/main/kotlin/com/example/theme/Color.kt
+++ app/src/main/kotlin/com/example/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue80 = Color(0xFFB3D8FF)       // Light Blue
+val BlueGrey80 = Color(0xFFB0C4DE)   // Light Steel Blue
+val Cyan80 = Color(0xFFAEEEEE)       // Pale Cyan
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue40 = Color(0xFF4682B4)       // Steel Blue
+val BlueGrey40 = Color(0xFF5F9EA0)   // Cadet Blue
+val Cyan40 = Color(0xFF008B8B)       // Dark Cyan
